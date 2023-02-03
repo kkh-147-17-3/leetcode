ddb17744e1b2e07692d6495c2cf7bcc4d094745d
kkh <102606939+kkh-147-17-3@users.noreply.github.com>
2023-02-03 10:36:47 +0900

    Update 0176-second-highest-salary.sql

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,15 +1,15 @@
 # Write your MySQL query statement below
 
-select max(
-        result2.salary
-    ) as SecondHighestSalary
-from
-    (select result1.salary 
-     from
+SELECT 
+     max(result2.salary) AS SecondHighestSalary
+FROM
+    (SELECT 
+        result1.salary 
+     FROM
         (SELECT 
             salary,
             DENSE_RANK() OVER (ORDER BY salary DESC) AS 'rank'
         FROM
             Employee ) result1
-    where result1.rank = 2) result2
+    WHERE result1.rank = 2) result2
 
